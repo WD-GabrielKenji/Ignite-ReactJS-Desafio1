4904466fecb1794269fa83e7327ad627de134548
WD-GabrielKenji <g.kenjiJS@hotmail.com>
2021-05-18 13:30:48 -0300

    Feat: Funcionabilidades de Adicionar e Remover Task(s) / README

---
+++ README.md
@@ -0,0 +1,101 @@
+<p align="center">
+   <img src="./public/logo.svg" alt="To.Do" width="300"/>
+</p>
+
+<p align="center">
+ <a href="#projeto">Projeto</a> •
+ <a href="#tecnologias">Tecnologias Utilizadas</a> •
+ <a href="#execucao">Como Executar</a> •
+ <a href="#documentacao">Documentação</a> •
+ <a href="#licenca">Licença</a> •
+ <a href="#autor">Autor</a> •
+ <a href="#contato">Contato</a> 
+</p>
+
+--------------------------- 
+
+<br>
+
+# :fire: <Strong> Obrigado por visualizar este repositório </Strong> <img src="https://user-images.githubusercontent.com/1303154/88677602-1635ba80-d120-11ea-84d8-d263ba5fc3c0.gif" width="28px" alt="hi">
+
+<p align="justify"> Primeiramente, olá me chamo Gabriel Kenji Poli Ozaki, sou um estudante de Ciência da Computação e estou querendo atuar na área de Desenvolvimento Web como Desenvolvedor Front-end. </p>
+
+## :computer: <strong id="projeto">Sobre o Projeto </strong>
+
+![alt](./public/Banner.png)
+
+<p align="justify"><strong>To.Do</strong> é um app voltado a um <strong>gerenciador de tarefas</strong> com opções de <strong>adicionar</strong> e <strong>remover</strong>.</p>
+
+<p align="justify"> Manter a organização em suas tarefas lhe possibilita gerenciar melhor seu tempo, ser mais produtivo e organizado! </p>
+
+<p align="justify">Essa aplicação To.Do foi desenvolvida, através do Bootcamp Ignite organizada pela empresa Rocketseat e mentoreado pelo Diego Fernandes na trilha de ReactJS.</p>
+
+## :rocket: <Strong id="tecnologias"> Tecnologias Utilizadas </Strong>
+
+<p align="justify"> Este projeto foi desenvolvido utilizando as seguintes tecnologias: </p>
+
+[![React Badge](https://img.shields.io/badge/-React-61DBFB?style=for-the-badge&labelColor=black&logo=react&logoColor=61DBFB)](https://reactjs.org/) [![Typescript Badge](https://img.shields.io/badge/-Typescript-007acc?style=for-the-badge&labelColor=black&logo=typescript&logoColor=007acc)](https://www.typescriptlang.org/) [![SASS Badge](https://img.shields.io/badge/-Sass-ff0080?style=for-the-badge&labelColor=black&logo=sass&logoColor=ff0080)](https://www.typescriptlang.org/)
+
+## :runner: <strong id="execucao"> Como executar o projeto </strong>
+
+[![Yarn Badge](https://img.shields.io/badge/yarn-1.22.5-brightgreen)](https://classic.yarnpkg.com/en/docs/install/#windows-stable)
+
+```bash
+# Fazer o clone do Repositorio.
+>> Git Clone: *A COLOCAR*
+```
+
+```bash
+# Necessario baixar as dependencias necessárias e iniciar o servidor de desenvolvimento:
+
+# Instalar as dependencias: 
+>> yarn
+
+# Iniciar o servidor de desenvolvimento:
+>> yarn dev
+```
+
+<pre>
+Agora basta acessar <a href="http://localhost:8080" rel="nofollow"><code>localhost:8080</code></a> no seu Browser e usufruir da aplicação!
+</pre>
+
+
+## :book: <strong id="documentacao"> Detalhes aprofundados sobre o projeto </strong>
+
+<details>
+<summary>
+  Documentação com os detalhes do projeto...
+</summary>
+
+<br>
+
+<h2><strong>Conceitos e Praticas utilizadas:</strong></h2>
+
+<p align="justify"> *Em construção* </p>
+
+</details>
+
+## :closed_book: <strong id="licenca"> Licença </strong>
+
+<img alt="GitHub" src="https://img.shields.io/github/license/facebook/react"/>
+
+Esse projeto esta sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para mais detalhes.
+
+## :boy: <strong id="autor"> Autor </strong>
+
+<a href="https://github.com/WD-GabrielKenji">
+ <img style="border-radius: 50%;" src="https://avatars.githubusercontent.com/u/77596710?s=400&u=70de2ffcac45b9e0db00c828fe785d4a76ac3f65&v=4" width="100px;" alt=""/>
+ <br />
+ <sub><b>Gabriel Kenji Poli Ozaki</b></sub></a> <a href="https://github.com/WD-GabrielKenji" title="Perfil Github"> :star2: 
+</a>
+
+
+Feito de ❤️ por Gabriel Kenji Poli Ozaki - Desenvolvedor Front-end 👋🏽
+
+### :mailbox_with_mail: <strong id="contato"> Redes Sociais para Contato: </strong>
+
+<p> Entre em contato, atraves destas redes sociais: </p>
+
+[![Linkedin Badge](https://img.shields.io/badge/-Gabriel_Kenji_Poli_Ozaki-0e76a8?style=flat&labelColor=0e76a8&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/wdkenji/)  [![Mail Badge](https://img.shields.io/badge/-@biel.kenjii-C63381?style=flat&labelColor=C63381&logo=instagram&logoColor=white)](https://www.instagram.com/biel.kenjii/)  [![Mail Badge](https://img.shields.io/badge/-g.kenjiJS-c0392b?style=flat&labelColor=c0392b&logo=gmail&logoColor=white)](mailto:g.kenjiJS@gmail.com)
+
+[![GitHub followers](https://img.shields.io/github/followers/WD-GabrielKenji.svg?style=social&label=Follow&maxAge=2592000)](https://github.com/WD-GabrielKenji)
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import '../styles/tasklist.scss'
+import "../styles/tasklist.scss";
 
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { FiTrash, FiCheckSquare } from "react-icons/fi";
 
 interface Task {
   id: number;
@@ -10,20 +10,56 @@ interface Task {
   isComplete: boolean;
 }
 
-export function TaskList() {
+export function TaskList(props: Task) {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [newTaskTitle, setNewTaskTitle] = useState("");
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const newTask = {
+      id: Math.floor(Math.random() * 1001),
+      title: newTaskTitle,
+      isComplete: false,
+    };
+
+    if (newTask.title === "") {
+      alert("Por favor, insira um todo valido");
+    } else {
+      setTasks((oldTask) => [...oldTask, newTask]);
+      setNewTaskTitle("");
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskCompleted = tasks.map((task) =>
+      task.id === id
+        ? {
+            ...task,
+            isComplete: !task.isComplete,
+          }
+        : task
+    );
+
+    setTasks(taskCompleted);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const identifierTask = tasks.filter((task) => task.id !== id);
+    setTasks(identifierTask);
+    console.log(id);
+  }
+
+  function removeAllTasks() {
+    setTasks([]);
+  }
+
+  function removeCompletedTasks() {
+    const fiterCompletedTasks = tasks.filter(
+      (task) => task.isComplete === false
+    );
+    setTasks(fiterCompletedTasks);
   }
 
   return (
@@ -32,25 +68,57 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            id="insertionField"
+            type="text"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
-          <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
-            <FiCheckSquare size={16} color="#fff"/>
+          <button
+            type="submit"
+            data-testid="add-task-button"
+            onClick={handleCreateNewTask}
+          >
+            <FiCheckSquare size={16} color="#fff" />
           </button>
         </div>
       </header>
 
       <main>
+        <div className="remove-tasks">
+          <div>
+            <button
+              type="button"
+              data-testid="removeAll-task-button"
+              onClick={() => removeAllTasks()}
+            >
+              <FiTrash size={16} />
+            </button>
+            <span>Apagar todos as Tarefas</span>
+          </div>
+
+          <div>
+            <button
+              type="button"
+              data-testid="removeCompleted-task-button"
+              onClick={() => removeCompletedTasks()}
+            >
+              <FiTrash size={16} />
+            </button>
+            <span>Apagar tarefas completas</span>
+          </div>
+        </div>
+
         <ul>
-          {tasks.map(task => (
+          {tasks.map((task) => (
             <li key={task.id}>
-              <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
+              <div
+                className={task.isComplete ? "completed" : ""}
+                data-testid="task"
+              >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -60,15 +128,17 @@ export function TaskList() {
                 </label>
                 <p>{task.title}</p>
               </div>
-
-              <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
-                <FiTrash size={16}/>
+              <button
+                type="button"
+                data-testid="remove-task-button"
+                onClick={() => handleRemoveTask(task.id)}
+              >
+                <FiTrash size={16} />
               </button>
             </li>
           ))}
-          
         </ul>
       </main>
     </section>
-  )
+  );
 }
